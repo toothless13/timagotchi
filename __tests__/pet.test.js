021fdc2756d46f5e4093c8fbf3ba0d9d73626ae4
Tim <50870607+toothless13@users.noreply.github.com>
2023-07-05 15:04:34 +0100

    add growUp() to Pet prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,9 +4,25 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    
     it('sets the name property',() => {
         const pet = new Pet('Fido');
 
         expect(pet.name).toEqual('Fido');
+    });
+    
+    it('has an age property', () => {
+        const pet = new Pet('Fido');
+
+        expect(typeof pet.age).toBe('number');
+        expect(pet.age).toBe(0);
+    });
+    
+    it('has a growUp method that increases age by 1', () => {
+        const pet = new Pet('Fido');
+        // The next line tested if growUp() was defined and returned a value
+        // expect(pet.growUp()).toBeDefined();
+        pet.growUp();
+        expect(pet.age).toBe(1);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,12 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+}
+
+Pet.prototype = {
+    growUp() {
+        this.age++;
+    }
 }
 
 module.exports = Pet;
